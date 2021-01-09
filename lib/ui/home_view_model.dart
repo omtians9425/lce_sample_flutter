25fbf3d76055a034a873e1ba9a6ad6ceecbd5390
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2021-01-10 03:46:30 +0900

    Create HomeViewModel

---
+++ lib/ui/home_view_model.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final homeViewModelProvider = ChangeNotifierProvider((ref) => HomeViewModel());
+
+class HomeViewModel extends ChangeNotifier {}
